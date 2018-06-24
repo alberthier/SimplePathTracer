20e7f61f070a5f22adb4a2a79bc381d55dec557d
Éric ALBER <eric.alber@gmail.com>
2018-06-24 23:48:21 +0200

    Add command line arguments

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"fmt"
 	"image/png"
 	"os"
+	"runtime/pprof"
 
 	"github.com/alberthier/pathtracer"
 )
 
 func main() {
+	width := flag.Int("width", 400, "Rendered image width")
+	height := flag.Int("height", 200, "Rendered image height")
+	samples := flag.Int("samples", 100, "Samples per pixel")
+	startframe := flag.Int("startframe", 1, "Animation start frame")
+	length := flag.Int("length", 1, "Animation length (frames)")
+	cpuprofile := flag.String("cpuprofile", "", "CPU profile file")
+	prefix := flag.String("prefix", "", "Output file prefix")
+
 	flag.Parse()
 
 	worldFile := flag.Arg(0)
@@ -19,25 +28,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	width := 400
-	height := 200
-	samples := 100
 	world := pathtracer.NewWorld()
-	err := world.Load(worldFile, float64(width)/float64(height))
+	err := world.Load(worldFile, float64(*width)/float64(*height))
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	/*
+	if len(*cpuprofile) != 0 {
 		f, _ := os.Create("cpuprofile")
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
-	*/
+	}
 
-	renderer := pathtracer.NewRenderer(width, height, samples)
-	for t := 0; t < 100; t++ {
+	renderer := pathtracer.NewRenderer(*width, *height, *samples)
+	for t := *startframe; t < (*startframe + *length); t++ {
 		img := renderer.Render(world, float64(t))
-		output, _ := os.Create(fmt.Sprintf("output%03d.png", t))
+		output, _ := os.Create(fmt.Sprintf("%s%03d.png", *prefix, t))
 		png.Encode(output, img)
 		output.Close()
 	}
